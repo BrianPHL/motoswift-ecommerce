302c39db467e9292042d8f2338e7ab9da625724a
Brian <brianpasco1206@gmail.com>
2025-04-11 23:44:27 +0800

    refactor(frontend): split ThemeContext.jsx for modularity

--- client/src/context/Theme/index.jsx
+++ client/src/context/Theme/index.jsx
@@ -1,9 +1,7 @@
-import { createContext, useContext, useState, useEffect } from "react";
-
-const ThemeContext = createContext();
+import { useContext, useState } from "react";
+import ThemeContext from "./context";
 
 export const ThemeProvider = ({ children }) => {
-
     const [ theme, setTheme ] = useState(() => localStorage.getItem("theme") || "light");
     const toggleTheme = () => {
         setTheme((previousTheme) => (previousTheme === "light" ? "dark" : "light"));
@@ -19,7 +17,6 @@ export const ThemeProvider = ({ children }) => {
             { children }
         </ThemeContext.Provider>
     );
-
 };
 
 export const useTheme = () => useContext(ThemeContext);
