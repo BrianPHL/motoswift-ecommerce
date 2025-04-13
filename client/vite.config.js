1d29e5d7217cd2221ec983c0a4cd6fd3af9f788f
Brian <brianpasco1206@gmail.com>
2025-04-13 23:15:16 +0800

    feat(node): add pathing aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import  { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   root: "./",
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, './src/assets'),
+      '@components': resolve(__dirname, './src/components'),
+      '@context': resolve(__dirname, './src/context'),
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@routes': resolve(__dirname, './src/routes'),
+      '@services': resolve(__dirname, './src/services'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
