be29786dea2aaa2eb7fdb073034f58f70e10cfeb
Brian <brianpasco1206@gmail.com>
2025-04-13 23:39:04 +0800

    fix(frontend): missing import in Theme context entry file

--- client/src/context/Theme/index.jsx
+++ client/src/context/Theme/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import ThemeContext from "./context";
 
 export const ThemeProvider = ({ children }) => {
