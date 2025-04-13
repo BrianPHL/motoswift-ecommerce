1ed354fdc1bb0085bd66f07150844c3d26becbde
Brian <brianpasco1206@gmail.com>
2025-04-13 23:36:04 +0800

    feat(frontend): add App theme & auth providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './context/Theme'
+import { AuthProvider } from './context/Auth'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 )
