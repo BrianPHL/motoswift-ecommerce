b2380e6b14f987c0931c733aad401868cf7490f8
Brian <brianpasco1206@gmail.com>
2025-04-13 23:37:41 +0800

    feat(frontend): add theme context at Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
+import { useTheme } from '@context/Theme';
 import Header from '@components/Header';
 import styles from './Home.module.css';
 
 const Home = () => {
 
+    const { theme, setTheme } = useTheme();
 
     return (
         <Header></Header>
