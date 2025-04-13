a37ede4e532e520072623fa20e57bdfa5591c434
Brian <brianpasco1206@gmail.com>
2025-04-13 23:33:03 +0800

    refactor(frontend): change home setup for modularity

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import Header from '@components/Header';
+import styles from './Home.module.css';
+
+const Home = () => {
+
+
+    return (
+        <Header></Header>
+    );
+
+}
+
+export default Home;
