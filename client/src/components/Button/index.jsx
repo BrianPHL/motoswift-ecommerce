1e2fa9ac0c8d4e6e6451ce310ef112c6e1a6a3cc
Brian <brianpasco1206@gmail.com>
2025-04-16 22:55:55 +0800

    feat(frontend): add initial Button component

---
+++ client/src/components/Button/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import styles from './Button.module.css';
+
+const Button = ({ label, type, action, icon, iconPosition, dropdownOptions }) => {
+
+    if (!label || !type) return null;
+
+    const [ isOpen, setIsOpen ] = useState(false);
+    const hasDropdown = dropdownOptions && dropdownOptions.length > 0;
+    const toggleDropdown = () => setIsOpen(!isOpen);
+    const renderComponent = () => {
+        if (hasDropdown) return (
+            <>
+                <button className={ styles[type] }>
+                    { label }
+                    <i className='fa-solid fa-chevron-down'></i>
+                </button>  
+            </>
+        );
+
+        if (icon && iconPosition) return (
+            <>
+               iconPosition === 'left' ? (
+                    <i className={ `${ icon }` }></i>
+                    { label }
+                ) : iconPosition === 'right' ? (
+                    { label }
+                    <i className={ `${ icon }` }></i>
+                );
+            </>
+        );
+
+        return label;
+
+    };
+        
+    return (
+        <button
+            className={ styles[type] }
+            onClick={ hasDropdown ? toggleDropdown : action }
+        >
+            { renderComponent() }
+        </button>
+    );
+};
+
+export default Button;
