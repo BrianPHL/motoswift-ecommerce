be32e8b490c50eb83d66e8d74e4cee4a9ef97384
Brian <brianpasco1206@gmail.com>
2025-04-17 15:23:54 +0800

    chore(frontend): cleanup unused imports/variables

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,7 @@
-import { useContext } from 'react';
-import { useTheme } from '@context/Theme';
 import Header from '@components/Header';
 import styles from './Home.module.css';
 
 const Home = () => {
-
-    const { theme, setTheme } = useTheme();
 
     return (
         <div className={ styles.home }>
