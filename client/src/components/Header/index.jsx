05a33c9998ee94a01e75e4b3c6e1facb8a95ca0f
Brian <brianpasco1206@gmail.com>
2025-04-18 22:23:41 +0800

    feat(frontend): add mobile Header for responsiveness

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -9,59 +9,99 @@ const Header = () => {
     const { theme, toggleTheme } = useTheme();
     
     return (
-        <div className={ styles.header }>
-            <div className={ styles.left }>
-                <Logo />
-                <Anchor
-                    label="About us"
-                    href="/about-us"
-                ></Anchor>
-            </div>
-            <div className={ styles.right }>
-                <div className={ styles.nav }>
+        <>
+            <div className={ styles['desktop-header'] }>
+                <div className={ styles['left'] }>
+                    <Logo />
                     <Anchor
-                        label="Home"
-                        href="/"
-                    ></Anchor>
-                    <Anchor
-                        id="motorcycleDropdown"
-                        label="Motorcycles"
-                        dropdownOptions={[
-                            {
-                                label: "Brand #1",
-                                href: "#",
-                            }
-                        ]}
-                    ></Anchor>
-                    <Anchor
-                        id="partsDropdown"
-                        label="Parts & Accessories"
-                        dropdownOptions={[
-                            {
-                                label: "Brand #1",
-                                href: "#",
-                            }
-                        ]}
-                    ></Anchor>
+                        label="About us"
+                        href="/about-us"
+                    />
                 </div>
-                <Button
-                    label="Sign in"
-                    type="secondary"
-                    action={ () => navigate('/sign-in') }
-                />
-                <Button
-                    type="icon"
-                    action={ () => navigate('/cart') }
-                    icon="fa-solid fa-cart-shopping"
-                />
-                <Button
-                    type="icon"
-                    action={ () => toggleTheme()  }
-                    icon={ theme === 'light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun' }
-                />
+                <div className={ styles['right'] }>
+                    <div className={ styles['nav'] }>
+                        <Anchor
+                            label="Home"
+                            href="/"
+                        />
+                        <Anchor
+                            id="motorcycleDropdown"
+                            label="Motorcycles"
+                            dropdownOptions={[
+                                {
+                                    label: "Brand #1",
+                                    href: "#",
+                                },
+                                {
+                                    label: "Brand #2",
+                                    href: "#",
+                                },
+                                {
+                                    label: "Brand #3",
+                                    href: "#",
+                                }
+                            ]}
+                        />
+                        <Anchor
+                            id="partsDropdown"
+                            label="Parts & Accessories"
+                            dropdownOptions={[
+                                {
+                                    label: "Brand #1",
+                                    href: "#",
+                                },
+                                {
+                                    label: "Brand #2",
+                                    href: "#",
+                                },
+                                {
+                                    label: "Brand #3",
+                                    href: "#",
+                                }
+                            ]}
+                        />
+                    </div>
+                    <Button
+                        label="Sign in"
+                        type="secondary"
+                        action={ () => navigate('/sign-in') }
+                    />
+                    <Button
+                        type="icon"
+                        action={ () => navigate('/cart') }
+                        icon="fa-solid fa-cart-shopping"
+                    />
+                    <Button
+                        type="icon"
+                        action={ () => toggleTheme()  }
+                        icon={ theme === 'light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun' }
+                    />
+                </div>
             </div>
-        </div>
-    )
+            <div className={ styles['mobile-header'] }>
+                <div className={ styles['left'] }>
+                    <Button
+                        type="icon"
+                        action={ () => setDrawerOpen(true) }
+                        icon="fa-solid fa-bars"
+                    />
+                    <Logo />
+                </div>
+                <div className={ styles['right'] }>
+                    <Button
+                        type="icon"
+                        action={ () => navigate('/cart') }
+                        icon="fa-solid fa-cart-shopping"
+                    />
+                    <Button
+                        type="icon"
+                        action={ () => toggleTheme()  }
+                        icon={ theme === 'light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun' }
+                    />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Header;
