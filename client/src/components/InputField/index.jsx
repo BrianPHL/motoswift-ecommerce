12ede8a252bc83ba742174011b4d65dc581d8e5d
Brian <brianpasco1206@gmail.com>
2025-04-20 03:11:22 +0800

    feat(components): add InputField component

---
+++ client/src/components/InputField/index.jsx
@@ -0,0 +1,30 @@
+import { Button } from '@components';
+import styles from './InputField.module.css';
+
+const InputField = ({ hint, icon }) => {
+    console.log(hint)
+
+    if (!hint) return null;
+
+    return (
+        <div className={ styles['wrapper'] }>
+            <input className={ icon ? styles['input-icon'] : styles['icon'] } type="text" placeholder={ hint }/>
+            { icon ? (
+                <Button
+                    type='icon'
+                    icon={ icon }
+                    action={ () => { console.log("test") } }
+                    isOutlined={ false }
+                />
+            ) : (
+                <Button
+                    label="Submit"
+                    type="primary"
+                    action={ () => console.log("Submit") }
+                />
+            )}
+        </div>
+    );
+};
+
+export default InputField;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
 export { default as Accordion } from "./Accordion";
 export { default as Header } from "./Header";
+export { default as InputField } from "./InputField";
