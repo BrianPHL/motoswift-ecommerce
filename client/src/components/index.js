c74b7ccbfa65c0ef65ea03afa4d04131d5079c87
Brian <brianpasco1206@gmail.com>
2025-04-19 16:08:17 +0800

    refactor(frontend): add Accordion to centralized exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,5 @@ export { default as Logo } from "./Logo";
 export { default as Anchor } from "./Anchor";
 export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
+export { default as Accordion } from "./Accordion";
 export { default as Header } from "./Header";
