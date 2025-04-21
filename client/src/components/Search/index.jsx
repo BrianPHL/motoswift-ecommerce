37ee5868bd133ccd418c32a39392388eaea2f8e8
Brian <brianpasco1206@gmail.com>
2025-04-21 23:06:03 +0800

    feat(components): add initial Search component

---
+++ client/src/components/Search/index.jsx
@@ -0,0 +1,7 @@
+import styles from './Search.module.css';
+
+const Search = ({}) => {
+
+};
+
+export default Search;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,3 +6,4 @@ export { default as Accordion } from "./Accordion";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as InputField } from "./InputField";
+export { default as Search } from "./Search";
