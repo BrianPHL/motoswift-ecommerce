ae97c5c95e17cd1367b0dad22b589715d8bcb65e
Brian <brianpasco1206@gmail.com>
2025-04-25 02:24:54 +0800

    feat(pages): modify initial MotorcycleStore page layout

--- client/src/pages/MotorcycleStore/index.jsx
+++ client/src/pages/MotorcycleStore/index.jsx
@@ -1,9 +1,11 @@
+import { TableHeader } from '@components';
 import styles from './MotorcycleStore.module.css';
 
 const MotorcycleStore = () => {
     return (
         <div className={ styles['wrapper'] }>
             <h1>This is the Motorcycle Store page.</h1>
+            <TableHeader tableName='motorcycles' />
         </div>
     );
 };
