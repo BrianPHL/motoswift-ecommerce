402d74b4d7aeeabad1cc3ee28fddaa194abc8847
Brian <brianpasco1206@gmail.com>
2025-04-26 01:44:04 +0800

    feat(pages): finish Sign In page

--- client/src/pages/SignIn/index.jsx
+++ client/src/pages/SignIn/index.jsx
@@ -1,9 +1,58 @@
+import { useNavigate } from 'react-router';
+import { Button, InputField } from '@components';
 import styles from './SignIn.module.css';
 
 const SignIn = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={ styles['wrapper'] }>
-            <h1>This is the Sign In page.</h1>
+            <div className={ styles['header'] }>
+                <Button
+                    icon='fa-solid fa-angle-left'
+                    type='secondary'
+                    label='Go back'
+                    iconPosition='left'
+                    action={ () => { navigate(-1) } }
+                />
+                <h1>Sign into your account</h1>
+            </div>
+            <div className={ styles['container'] }>
+                <div className={ styles['banner'] }></div>
+                <form className={ styles['form'] }>
+                    <div className={ styles['inputs-container'] }>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="email_address">
+                                Email address
+                            </label>
+                            <InputField
+                                hint='Your email address...'
+                                type='email'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <InputField
+                                hint='Your password...'
+                                type='password'
+                                icon='fa-solid fa-eye-slash'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                    </div>
+                    <div className={ styles['ctas-container'] }>
+                        <Button
+                            type='primary'
+                            label='Sign in'
+                            action={ () => {} }
+                        />
+                        <p>Don't have an account yet? <span className={ styles['sign-up'] }>Sign up</span>.</p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
