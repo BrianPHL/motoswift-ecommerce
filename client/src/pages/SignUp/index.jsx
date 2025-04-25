1ac7a493a6e3746a0abe631f00ea083c97349534
Brian <brianpasco1206@gmail.com>
2025-04-26 02:31:47 +0800

    feat(pages): finish Sign Up page

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,11 +1,81 @@
+import { useNavigate } from 'react-router';
+import { Anchor, Button, InputField } from '@components';
 import styles from './SignUp.module.css';
 
-const SignUp = () => {
+const SignIn = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={ styles['wrapper'] }>
-            <h1>This is the Sign Up page.</h1>
+            <div className={ styles['header'] }>
+                <Button
+                    icon='fa-solid fa-angle-left'
+                    type='secondary'
+                    label='Go back'
+                    iconPosition='left'
+                    action={ () => { navigate(-1) } }
+                />
+                <h1>Create your account</h1>
+            </div>
+            <div className={ styles['container'] }>
+                <div className={ styles['banner'] }></div>
+                <form className={ styles['form'] }>
+                    <div className={ styles['inputs-container'] }>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="name">
+                                Name
+                            </label>
+                            <InputField
+                                hint='Your name...'
+                                type='text'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="email_address">
+                                Email address
+                            </label>
+                            <InputField
+                                hint='Your email address...'
+                                type='email'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <InputField
+                                hint='Your password...'
+                                type='password'
+                                icon='fa-solid fa-eye-slash'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                        <div className={ styles['input-wrapper'] }>
+                            <label htmlFor="confirm_password">
+                                Confirm password
+                            </label>
+                            <InputField
+                                hint='Confirm your password...'
+                                type='password'
+                                icon='fa-solid fa-eye-slash'
+                                isSubmittable={ false }
+                            />
+                        </div>
+                    </div>
+                    <div className={ styles['ctas-container'] }>
+                        <Button
+                            type='primary'
+                            label='Sign up'
+                            action={ () => {} }
+                        />
+                        <p>Already have an account? <Anchor label="Sign in" href="/sign-in" isNested={ false }/></p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
 
-export default SignUp;
+export default SignIn;
