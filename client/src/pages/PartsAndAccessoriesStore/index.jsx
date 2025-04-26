9a6ff5a63cbc962e92a6ed2aaf80375276d5b7fe
CjayA18 <qc-amancio@tip.edu.ph>
2025-04-26 19:26:21 +0800

    Changes in parts-and-accessories page (no product cards yet)

--- client/src/pages/PartsAndAccessoriesStore/index.jsx
+++ client/src/pages/PartsAndAccessoriesStore/index.jsx
@@ -1,10 +1,57 @@
+import { TableHeader, Button, ProductCard } from '@components';
 import styles from './PartsAndAccessoriesStore.module.css';
+import { useNavigate } from 'react-router';
+
 
 const PartsAndAccessoriesStore = () => {
+    const navigate = useNavigate();
     return (
-        <div className={ styles['wrapper'] }>
-            <h1>This is the Parts and Accessories Store page.</h1>
-        </div>
+
+        
+        
+        
+        <div className={styles['wrapper']}>
+            
+                
+                <div className={ styles['banner'] }>
+            </div>
+            
+            
+             <span className={styles['pagewrap']}>
+             
+            
+             <Button
+                        icon='fa-solid fa-angle-left'
+                        type='secondary'
+                        label='Parts and Accessories'
+                        action={ () => navigate (-1)}
+                        iconPosition='left' >
+                            
+                        </Button>
+              
+
+                </span>
+
+                
+                <h1>Find The Perfect Parts For Your Ride</h1>
+                <TableHeader tableName='parts-and-accessories' />
+
+                <div className={styles['container']}>
+                <div className={styles['products']}>
+                <ProductCard
+                    tags={['Hot', 'Adventure', 'XD']}
+                    name="Honda Beat (Premium)"
+                    price="PHP123,456.78"
+                    description="108cc 4-stroke single | 8.8hp | 9.21Nm"
+                    imageSrc="https://via.placeholder.com/186x174?text=Product" // optional placeholder
+                    onReserve={() => console.log('Reserved')}
+                    onCart={() => console.log('Added to cart')}
+                />
+                </div>
+           
+            </div>
+         </div>
+        
     );
 };
 
