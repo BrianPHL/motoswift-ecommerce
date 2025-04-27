07a7abf3ea1ac690b73119119117358fc36d43bc
gushiees <ivanabaa19@gmail.com>
2025-04-27 20:51:17 +0800

    Motorcycle Details and Parts and Accessories Details

--- client/src/pages/MotorcycleDetails/index.jsx
+++ client/src/pages/MotorcycleDetails/index.jsx
@@ -1,4 +1,4 @@
-import { TableHeader, Button, ProductCard } from '@components';
+import { Button } from '@components';
 import styles from './MotorcycleDetails.module.css';
 import { useNavigate } from 'react-router';
 
@@ -6,7 +6,35 @@ const MotorcycleDetails = () => {
     const navigate = useNavigate();
     return (
         <div className={styles['wrapper']}>
-            <h1>This is Motorcycle Product page</h1>
+            <div className={styles['container']}>
+                <div className={styles['imageContainer']}>
+                    <img></img>
+                </div>
+                <div className={styles['Details']}>
+                    <div className={styles['productDescription']}>
+                        <h1>The New CLICK125</h1>
+                        <h2>Brand: Honda | Category: Scooters</h2>
+                        <p>Unleash #The Ultimate Game Changer with The New Click125 showcasing a fresh design featuring striking new two-tone colors and dynamic stripes for the Click125 Standard Variant, while complemented by a sophisticated 3D Emblem exclusive to Special Edition Variant.
+
+                        The New Click125 Standard variant comes in Four striking colors available in:
+
+                        Pearl Arctic White
+                        Pearl Sylvestris Gray
+                        Stellar Blue Metallic
+                        Obsidian Black Metallic</p>
+
+                        <h1>PHP123,456.78</h1>
+                    </div>
+                    <div className={styles['buttonContainer']}>
+                        <div className={styles['reserveButton']}>
+                        <Button label="Reserve Now" type="primary" action={() => console.log('Submit')} />
+                        </div>
+                        <div className={styles['cartIcon']}>
+                        <Button label="Add to Cart" type="secondary" action={() => {}} icon="fa-solid fa-cart-shopping" iconPosition="left" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
--- client/src/pages/PartsAndAccessoriesDetails/index.jsx
+++ client/src/pages/PartsAndAccessoriesDetails/index.jsx
@@ -1,4 +1,4 @@
-import { TableHeader, Button, ProductCard } from '@components';
+import { Button } from '@components';
 import styles from './PartsAndAccessoriesDetails.module.css';
 import { useNavigate } from 'react-router';
 
@@ -6,7 +6,32 @@ const PartsAndAccessoriesDetails = () => {
     const navigate = useNavigate();
     return (
         <div className={styles['wrapper']}>
-            <h1>This is Parts Product page</h1>
+            <div className={styles['container']}>
+                <div className={styles['imageContainer']}>
+                    <img></img>
+                </div>
+                <div className={styles['Details']}>
+                    <div className={styles['productDescription']}>
+                        <h1>Krator Universal Black Motorcycle Mirrors for Street Bike, Sportbike, Naked Bike, Cruiser, Chopper, Scooter, Moped, ATV (8mm + 10mm Bolt Adapters) - Pair</h1>
+                        <h2>Brand: Kratos | Category: Motor Parts</h2>
+                        <p>High Quality Black Mirrors (Left & Right Sides)
+                        Adjustable Mirror Angle, Up and Down, Left and Right
+                        Quantity: 1 Pair (Left and Right)
+                        Brand: Krator | Color: Black
+                        Universal Mirrors will fit the vast majority of motorcycles including Honda, Suzuki, Yamaha, Kawasaki, etc.</p>
+
+                        <h1>PHP123,456.78</h1>
+                    </div>
+                    <div className={styles['buttonContainer']}>
+                        <div className={styles['reserveButton']}>
+                        <Button label="Reserve Now" type="primary" action={() => console.log('Submit')} />
+                        </div>
+                        <div className={styles['cartIcon']}>
+                        <Button label="Add to Cart" type="secondary" action={() => {}} icon="fa-solid fa-cart-shopping" iconPosition="left" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
