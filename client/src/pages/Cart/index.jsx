4ea04af1ce6595550f400d7aa127f48ae8d0fa8a
Brian <brianpasco1206@gmail.com>
2025-04-30 02:23:38 +0800

    refactor(pages): hide summary when cart is empty

--- client/src/pages/Cart/index.jsx
+++ client/src/pages/Cart/index.jsx
@@ -29,97 +29,99 @@ const Cart = () => {
                         <p>Start browsing for items in <Anchor label="Motorcycles" link="/motorcycles" isNested={ false }/> or <Anchor label="Parts & Accessories" link="/parts-and-accessories" isNested={ false }/>.</p>
                     </div>
                 ) : (
-                    <div className={ styles['cart'] }>
-                        <div className={ styles['cart-header'] }>
-                            <h3>Product</h3>
-                            <h3>Quantity</h3>
-                            <h3>Price</h3>
-                            <h3>Total</h3>
-                        </div>
-                        <div className={ styles['divider'] }></div>
-                        { cartItems.map(item => (
-                            <div className={ styles['cart-item'] } key={ item.id }>
-                                <div className={ styles['cart-item-product'] }>
-                                    <img src={ item.img } alt={ item.label } />
-                                    <div className={ styles['cart-item-product-details'] }>
-                                        <div className={ styles['cart-item-product-details-info'] }>
-                                            <h3>{ item.category } ({ item.subcategory })</h3>
-                                            <h2>{ item.label }</h2>
-                                        </div>
-                                        <div className={ styles['cart-item-product-details-cta'] }>
-                                            <Button
-                                                type='secondary'
-                                                label='View item'
-                                                icon='fa-solid fa-square-up-right'
-                                                iconPosition='right'
-                                                action={ () => navigate(`/motorcycles/${item.id}`) }
-                                            />
-                                            <Button
-                                                type='icon-outlined'
-                                                icon='fa-solid fa-trash-can'
-                                                action={ () => removeFromCart(item.id) }
-                                            />
+                    <>
+                        <div className={ styles['cart'] }>
+                            <div className={ styles['cart-header'] }>
+                                <h3>Product</h3>
+                                <h3>Quantity</h3>
+                                <h3>Price</h3>
+                                <h3>Total</h3>
+                            </div>
+                            <div className={ styles['divider'] }></div>
+                            { cartItems.map(item => (
+                                <div className={ styles['cart-item'] } key={ item.id }>
+                                    <div className={ styles['cart-item-product'] }>
+                                        <img src={ item.img } alt={ item.label } />
+                                        <div className={ styles['cart-item-product-details'] }>
+                                            <div className={ styles['cart-item-product-details-info'] }>
+                                                <h3>{ item.category } ({ item.subcategory })</h3>
+                                                <h2>{ item.label }</h2>
+                                            </div>
+                                            <div className={ styles['cart-item-product-details-cta'] }>
+                                                <Button
+                                                    type='secondary'
+                                                    label='View item'
+                                                    icon='fa-solid fa-square-up-right'
+                                                    iconPosition='right'
+                                                    action={ () => navigate(`/motorcycles/${item.id}`) }
+                                                />
+                                                <Button
+                                                    type='icon-outlined'
+                                                    icon='fa-solid fa-trash-can'
+                                                    action={ () => removeFromCart(item.id) }
+                                                />
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className={ styles['cart-item-quantity'] }>
+                                        <InputField
+                                            hint='Qty...'
+                                            isSubmittable={ false }
+                                            type='number'
+                                            value={ item.quantity }
+                                            min={1}
+                                            onChange={e => updateQuantity( item.id, e.target.value )}
+                                        />
+                                    </div>
+                                    <div className={ styles['cart-item-price'] }>
+                                        <h3>{ item.price }</h3>
+                                    </div>
+                                    <div className={ styles['cart-item-total'] }>
+                                        <h3>
+                                            ₱ {(Number(item.price.replace(/[^\d]/g, "")) * item.quantity).toLocaleString()}
+                                        </h3>
+                                    </div>
                                 </div>
-                                <div className={ styles['cart-item-quantity'] }>
-                                    <InputField
-                                        hint='Qty...'
-                                        isSubmittable={ false }
-                                        type='number'
-                                        value={ item.quantity }
-                                        min={1}
-                                        onChange={e => updateQuantity( item.id, e.target.value )}
-                                    />
+                            ))}
+                        </div>
+                        <div className={ styles['summary'] }>
+                            <h2>Summary</h2>
+                            <div className={ styles['divider'] }></div>
+                            <div className={ styles['summary-wrapper'] }>
+                                <div className={ styles['summary-item'] }>
+                                    <h3>Sub-total</h3>
+                                    <h3>₱{ subtotal.toLocaleString() }</h3>
                                 </div>
-                                <div className={ styles['cart-item-price'] }>
-                                    <h3>{ item.price }</h3>
+                                <div className={ styles['summary-item'] }>
+                                    <h3>Tax</h3>
+                                    <h3>₱{ tax.toLocaleString() }</h3>
                                 </div>
-                                <div className={ styles['cart-item-total'] }>
-                                    <h3>
-                                        ₱ {(Number(item.price.replace(/[^\d]/g, "")) * item.quantity).toLocaleString()}
-                                    </h3>
+                                <div className={ styles['summary-item'] }>
+                                    <h3>Deductions</h3>
+                                    <h3>₱{ deductions.toLocaleString() }</h3>
+                                </div>
+                                <div className={ styles['summary-item'] }>
+                                    <h3>Total</h3>
+                                    <h3>₱{ total.toLocaleString() }</h3>
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                            <div className={ styles['divider'] }></div>
+                            <div className={ styles['cta'] }>
+                                <Button
+                                    type='primary'
+                                    label='Proceed to Checkout'
+                                    action={ () => {} }
+                                    disabled
+                                />
+                                <Button
+                                    type='primary'
+                                    label='Proceed to Reservation'
+                                    action={ () => {} }
+                                />
+                            </div>
+                        </div>
+                    </>
                 )}
-                <div className={ styles['summary'] }>
-                    <h2>Summary</h2>
-                    <div className={ styles['divider'] }></div>
-                    <div className={ styles['summary-wrapper'] }>
-                        <div className={ styles['summary-item'] }>
-                            <h3>Sub-total</h3>
-                            <h3>₱{ subtotal.toLocaleString() }</h3>
-                        </div>
-                        <div className={ styles['summary-item'] }>
-                            <h3>Tax</h3>
-                            <h3>₱{ tax.toLocaleString() }</h3>
-                        </div>
-                        <div className={ styles['summary-item'] }>
-                            <h3>Deductions</h3>
-                            <h3>₱{ deductions.toLocaleString() }</h3>
-                        </div>
-                        <div className={ styles['summary-item'] }>
-                            <h3>Total</h3>
-                            <h3>₱{ total.toLocaleString() }</h3>
-                        </div>
-                    </div>
-                    <div className={ styles['divider'] }></div>
-                    <div className={ styles['cta'] }>
-                        <Button
-                            type='primary'
-                            label='Proceed to Checkout'
-                            action={ () => {} }
-                            disabled
-                        />
-                        <Button
-                            type='primary'
-                            label='Proceed to Reservation'
-                            action={ () => {} }
-                        />
-                    </div>
-                </div>
             </div>
         </div>
     );
