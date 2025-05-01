a190627f057b7540d552ac21c8656a2dc3f52665
Brian <brianpasco1206@gmail.com>
2025-05-01 16:51:15 +0800

    fix(server): database not connected due to missing credentials

--- server/db.js
+++ server/db.js
@@ -6,7 +6,11 @@ import { Pool } from "pg";
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 });
 
 export default pool;
