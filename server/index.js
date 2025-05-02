da3d44624fbe34425840b183e70d3393ae0a7191
Brian <brianpasco1206@gmail.com>
2025-05-02 22:04:29 +0800

    fix(server): frontend is not being served by backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import cors from 'cors';
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
 import accountRoutes from './account.route.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +17,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/accounts', accountRoutes);
 
-app.get('/', (req, res) => res.send("Backend running!"));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    if (req.path.startsWith('/api/')) return res.status(404).end();
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${ PORT }`)
