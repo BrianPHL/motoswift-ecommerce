eb1e48194a8e1b5d6397947d1bb7ccab643fd7e0
Brian <brianpasco1206@gmail.com>
2025-05-02 21:54:59 +0800

    chore(server): prepare for fullstack hosting

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import accountRoutes from './account.route.js';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
