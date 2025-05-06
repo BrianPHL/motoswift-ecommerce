2702423c148a4e20d196dd0c3c5d6efbaa9cd5e3
Brian <brianpasco1206@gmail.com>
2025-05-06 22:59:03 +0800

    fix(components): fad796b continuation

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -149,7 +149,7 @@ const Header = () => {
                     <div className={ styles['right'] }>
                     { user && user['role'] === 'customer' ? (
                         <Button
-                            id='account-desktop-dropdown'
+                            id='account-mobile-dropdown'
                             type='secondary'
                             label={ user['first_name'] }
                             dropdownPosition='right'
@@ -166,7 +166,7 @@ const Header = () => {
                         />
                     ) : user && user['role'] === 'admin' ? (
                         <Button
-                            id='account-desktop-dropdown-2'
+                            id='account-mobile-dropdown-2'
                             type='secondary'
                             label={ user['first_name'] }
                             dropdownPosition='right'
