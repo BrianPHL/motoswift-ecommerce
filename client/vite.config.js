c6be4fee23108b458dccd845c350f2921b757217
Brian <brianpasco1206@gmail.com>
2025-05-18 09:58:41 +0800

    chore(vite): cleanup of unused path aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,13 +8,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@assets': resolve(__dirname, './src/assets'),
       '@components': resolve(__dirname, './src/components'),
       '@contexts': resolve(__dirname, './src/contexts'),
-      '@layouts': resolve(__dirname, './src/layouts'),
       '@pages': resolve(__dirname, './src/pages'),
       '@routes': resolve(__dirname, './src/routes'),
-      '@services': resolve(__dirname, './src/services'),
     },
   },
   server: {
