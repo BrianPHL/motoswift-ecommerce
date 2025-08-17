37efcd0091fae3e0af233ce6109dad80b26acfc7
Brian <brianpasco1206@gmail.com>
2025-08-17 22:57:10 +0800

    cleanup(utils): remove unneeded validation.js utility functions

--- client/src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-import { OTP_LENGTH } from './constants.js';
-
-export const isValidOTP = (otp) => {
-    return otp && otp.length === OTP_LENGTH && /^\d+$/.test(otp);
-};
-
-export const cleanOTPInput = (input) => {
-    return input.replace(/\D/g, '').slice(0, OTP_LENGTH);
-};
